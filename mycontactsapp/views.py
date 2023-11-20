bbfdd20e476a8c269ba474ee3975e370be2be4ba
shirisha24-hub <ammusiri242@gmail.com>
2023-11-20 23:47:44 +0300

    modified views

--- mycontactsapp/views.py
+++ mycontactsapp/views.py
@@ -6,16 +6,7 @@ from .models import Contact
 
 
 def index(request):
-    contacts_list = Contact.objects.all().order_by("first_name", "last_name")
+    # Display contacts
+    all_contacts = Contact.objects.all()
 
-    formatted_list = "<br>".join(
-        [f"{contact.first_name} {contact.last_name}" for contact in contacts_list]
-    )
-
-    centered_list = f'<div style="text-align: center;">{formatted_list}</div>'
-
-    styled_response = (
-        f"<style>body {{ background-color: #f0f0f0; }}</style>{centered_list}"
-    )
-
-    return HttpResponse("All Contacts:<br>" + styled_response)
+    return render(request, "index.html", {"contacts": all_contacts})
